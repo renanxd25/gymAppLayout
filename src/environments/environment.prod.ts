17a0864920a9c5e00bc7237f6992fce967c68f9c
renanxd25 <renanxd7x@hotmail.com>
2023-07-21 18:17:17 -0300

    adicionando integracao do firebese em prod para o deploy do vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDv3g5xPhqKZTSNZ7b8ywXAcoEko5H2Hfo",
+    authDomain: "ionictesteb2b.firebaseapp.com",
+    projectId: "ionictesteb2b",
+    storageBucket: "ionictesteb2b.appspot.com",
+    messagingSenderId: "150528207433",
+    appId: "1:150528207433:web:5144a666ef51d6b7536d98",
+    measurementId: "G-2R89VLD3FT"
+  },
 };
